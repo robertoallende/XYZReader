aa4fe9529ad0608c59639728c46022c513653454
robertoallende <rallende@gmail.com>
2016-01-11 20:04:05 -0300

    Refactor:  using CoordinatorLayout and AppBarLayout instead of custom stuff. Adding FAB Icon too.

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.text.format.DateUtils;
+
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -42,9 +43,6 @@ public class ArticleListActivity extends ActionBarActivity implements
         setContentView(R.layout.activity_article_list);
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
-
-
-        final View toolbarContainerView = findViewById(R.id.toolbar_container);
 
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
 
