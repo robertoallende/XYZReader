4cc36485cb470bac3b743100a945c4d5a023530c
robertoallende <rallende@gmail.com>
2016-01-14 11:07:35 -0300

    Refactor: Adding Gradle Flavors to divide the old and new UI.

---
+++ XYZReader/src/newui/java/com/example/xyzreader/newui/ArticleListActivity.java
@@ -0,0 +1,15 @@
+package com.example.xyzreader.newui;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.example.xyzreader.R;
+
+public class ArticleListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article_list);
+    }
+}
